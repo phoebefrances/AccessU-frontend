0376f7633e08546b7e33ffb88a5a3cb462cd33be
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-07-27 16:03:40 +0100

    created a stylesheet for the SearchBar component

--- frontend/Components/SearchBar/SearchBar.js
+++ frontend/Components/SearchBar/SearchBar.js
@@ -1,109 +1,21 @@
-// import React from "react";
-// import Box from "@mui/material/Box";
-// import TextField from "@mui/material/TextField";
-
-// export default function BasicTextFields() {
-//   return (
-//     <Box
-//       component="form"
-//       sx={{
-//         "& > :not(style)": { m: 1, width: "25ch" },
-//       }}
-//       noValidate
-//       autoComplete="off"
-//     >
-//       {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-//       <TextField id="filled-basic" label="Filled" variant="filled" /> */}
-//       <TextField id="standard-basic" label="Standard" variant="standard" />
-//     </Box>
-//   );
-// }
-
-
-// import * as React from 'react';
-// import Box from '@mui/material/Box';
-// import TextField from '@mui/material/TextField';
-// import MenuItem from '@mui/material/MenuItem';
-
-// const currencies = [
-//   {
-//     value: 'Cinema',
-//     label: 'Cinema',
-//   },
-//   {
-//     value: '',
-//     label: 'Restaurant',
-//   },
-//   {
-//     value: '',
-//     label: 'Gym',
-//   },
-//   {
-//     value: '',
-//     label: 'Shop',
-//   },
-//   {
-//     value: '',
-//     label: 'Cafe',
-//   },
-// ];
-
-// export default function SelectTextFields() {
-//   const [currency, setCurrency] = React.useState('EUR');
-
-//   const handleChange = (event) => {
-//     setCurrency(event.target.value);
-//   };
-
-//   return (
-//     <Box
-//       component="form"
-//       sx={{
-//         '& .MuiTextField-root': { m: 1, width: '25ch' },
-//       }}
-//       noValidate
-//       autoComplete="off"
-//     >
-   
-//       <div>
-//         <TextField
-//           id="standard-select-currency"
-//           select
-//           label="Select"
-//           value={currency}
-//           onChange={handleChange}
-//           helperText="Please select your currency"
-//           variant="standard"
-//         >
-//           {currencies.map((option) => (
-//             <MenuItem key={option.value} value={option.value}>
-//               {option.label}
-//             </MenuItem>
-//           ))}
-//         </TextField>
-       
-//       </div>
-//     </Box>
-//   );
-// }
-
-
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+import styles from "./SearchBarStyles.module.css";
 
 export default function BasicSelect() {
-  const [category, setCategory] = React.useState('');
+  const [category, setCategory] = React.useState("");
 
   const handleChange = (event) => {
     setCategory(event.target.value);
   };
 
   return (
-    <Box sx={{ minWidth: 120 }}>
+    <Box sx={{ minWidth: 120 }} className={styles.global}>
       <FormControl fullWidth>
         <InputLabel id="demo-simple-select-label">Category</InputLabel>
         <Select
@@ -113,13 +25,31 @@ export default function BasicSelect() {
           label="Category"
           onChange={handleChange}
         >
-          <MenuItem value={'Cinema'}>Cinema</MenuItem>
-          <MenuItem value={'Restaurant'}>Restaurant</MenuItem>
-          <MenuItem value={'Gym'}>Gym</MenuItem>
-          <MenuItem value={'Shop'}>Shop</MenuItem>
-          <MenuItem value={'Cafe'}>Cafe</MenuItem>
+          <MenuItem value={"Cinema"}>Cinema</MenuItem>
+          <MenuItem value={"Restaurant"}>Restaurant</MenuItem>
+          <MenuItem value={"Gym"}>Gym</MenuItem>
+          <MenuItem value={"Shop"}>Shop</MenuItem>
+          <MenuItem value={"Cafe"}>Cafe</MenuItem>
         </Select>
       </FormControl>
+      <TextField id="standard-basic" label="Standard" variant="standard" />
     </Box>
   );
 }
+
+// export default function BasicTextFields() {
+//     return (
+//       <Box
+//         component="form"
+//         sx={{
+//           '& > :not(style)': { m: 1, width: '25ch' },
+//         }}
+//         noValidate
+//         autoComplete="off"
+//       >
+//         <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+//         <TextField id="filled-basic" label="Filled" variant="filled" />
+//         <TextField id="standard-basic" label="Standard" variant="standard" />
+//       </Box>
+//     );
+//   }
