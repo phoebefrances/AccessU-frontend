32e74879b9f51f37124cf5a4d70df1282e1f6f31
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-07-28 10:48:57 +0100

    added a Box over our search bar with a border

--- frontend/Components/SearchBar/SearchBar.js
+++ frontend/Components/SearchBar/SearchBar.js
@@ -6,12 +6,12 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import TextField from "@mui/material/TextField";
 import styles from "./SearchBarStyles.module.css";
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 import { red } from "@mui/material/colors";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
 
-// Custom Component containing category drop down menu, location search input field and search button 
+// Custom Component containing category drop down menu, location search input field and search button
 
 export default function BasicSelect() {
   const [category, setCategory] = React.useState("");
@@ -21,36 +21,35 @@ export default function BasicSelect() {
   };
 
   return (
-    <Stack direction="row" sx={{borderColor: '#ff0099'}} className={styles.global} >
+    <Box className={styles.global}>
+      <Stack direction="row">
+        {/* Category drop down menu */}
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Category</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={category}
+            label="Category"
+            onChange={handleChange}
+          >
+            <MenuItem value={"Cinema"}>Cinema</MenuItem>
+            <MenuItem value={"Restaurant"}>Restaurant</MenuItem>
+            <MenuItem value={"Gym"}>Gym</MenuItem>
+            <MenuItem value={"Shop"}>Shop</MenuItem>
+            <MenuItem value={"Cafe"}>Cafe</MenuItem>
+          </Select>
+        </FormControl>
 
-      {/* Category drop down menu */}
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Category</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={category}
-          label="Category"
-          onChange={handleChange}
-        >
-          <MenuItem value={"Cinema"}>Cinema</MenuItem>
-          <MenuItem value={"Restaurant"}>Restaurant</MenuItem>
-          <MenuItem value={"Gym"}>Gym</MenuItem>
-          <MenuItem value={"Shop"}>Shop</MenuItem>
-          <MenuItem value={"Cafe"}>Cafe</MenuItem>
-        </Select>
-      
-      </FormControl>
+        {/* Search Location input field  */}
+        <TextField id="standard-basic" label="Standard" variant="standard" />
 
-      {/* Search Location input field  */}
-      <TextField id="standard-basic" label="Standard" variant="standard" />
+        {/* Search button  */}
 
-       {/* Search button  */}
-      <Stack direction="column" spacing={4} >
-      <Button variant="contained" startIcon={<SearchIcon />}>Search</Button>
-  
-    </Stack>
-    </Stack>
+        <Button variant="contained" startIcon={<SearchIcon />}>
+          Search
+        </Button>
+      </Stack>
+    </Box>
   );
 }
-
