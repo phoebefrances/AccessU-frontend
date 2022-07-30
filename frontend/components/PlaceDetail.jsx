46ae7a7d17c6b50e8a7bde2f9db179934a92328f
Phoebe Windham <phoebe.frances@outlook.com>
2022-07-30 14:40:46 +0100

    created main card component for places

--- frontend/components/PlaceDetail.jsx
+++ frontend/components/PlaceDetail.jsx
@@ -1,7 +1,19 @@
+import { Flex } from "@chakra-ui/react";
 import React from "react";
 
 const PlaceDetail = ({ place }) => {
-  return <div>{place.name}</div>;
+  return <Flex
+  bg={'whiteAlpha.100.900'}
+  px={4}
+  py={2}
+  mb={2}
+  shadow='lg'
+  direction={'column'}
+  alignItems={'start'}
+  justifyContent='space-evenly'
+  >
+  
+  </Flex>
 };
 
 export default PlaceDetail;
