d149e879001c2478974e323d9b0c871d35ee2dc5
Phoebe Windham <phoebe.frances@outlook.com>
2022-07-30 15:08:00 +0100

    added image for restuarants using the place object and added a place image holder encase the image isn't available

--- frontend/components/PlaceDetail.jsx
+++ frontend/components/PlaceDetail.jsx
@@ -1,53 +1,63 @@
-import { Flex } from "@chakra-ui/react";
+
+import { Image, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
 const PlaceDetail = ({ place }) => {
-  return <Flex
-  bg={'whiteAlpha.100.900'}
-  px={4}
-  py={2}
-  mb={2}
-  shadow='lg'
-  direction={'column'}
-  alignItems={'start'}
-  justifyContent='space-evenly'
-  >
+  return (
     <Flex
-    justifyContent={'space-between'}
-    width='full'
+      bg={"whiteAlpha.900"}
+      px={4}
+      py={2}
+      mb={2}
+      shadow="lg"
+      direction={"column"}
+      alignItems={"start"}
+      justifyContent="space-between"
     >
-      <Flex
-      direction={'column'}
-      justifyContent={'start'}
-      alignItems={'start'}
-      width='full'
-      px={2}
-      >
-      <Flex 
-      alignItems={'center'}
-      width={'full'}
-      justifyContent={'space-between'}
-      >
-        <Text 
-        textTransform={'capitalize'}
-        width={'40'}
-        fontSize={'lg'}
-        fontWeight={'500'}
-        isTruncated
+      <Flex justifyContent={"space-between"} width="full">
+        <Flex
+          direction={"column"}
+          justifyContent={"start"}
+          alignItems={"start"}
+          width="full"
+          px={2}
         >
-          {place.name}
-        </Text>
-        <Text
-        fontSize={'sm'}
-        fontWeight={'500'}
-        color={'gray.500'}
-        >
+          <Flex
+            alignItems={"center"}
+            width={"full"}
+            justifyContent={"space-between"}
+          >
+            <Text
+              textTransform={"capitalize"}
+              width={"40"}
+              fontSize={"lg"}
+              fontWeight={"500"}
+            >
+              {place.name}
+            </Text>
 
-        </Text>
+            <Text fontSize={"sm"} fontWeight={"500"} color={"gray.500"}>
+              {place.price}
+            </Text>
+          </Flex>
+
+          
+        </Flex>
+        
       </Flex>
-      </Flex>
+      <Image
+          objectFit={"cover"}
+          width={"120px"}
+          height={"120px"}
+          rounded="lg"
+          src={
+            place.photo
+              ? place.photo.images.large.url
+              : "https://explorelompoc.com/wp-content/uploads/2021/06/food_placeholder.jpg"
+          }
+        />
     </Flex>
-  </Flex>
+  );
 };
 
 export default PlaceDetail;
