cd56456932fdee50bed3670d936a7c0cd2ab6864
Phoebe Windham <phoebe.frances@outlook.com>
2022-07-30 09:09:51 +0100

    api data is being fetched and logged to the console

---
+++ frontend/pages/api/index.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const url =  'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+const options = {
+  
+  params: {
+    bl_latitude: '11.847676',
+    tr_latitude: '12.838442',
+    bl_longitude: '109.095887',
+    tr_longitude: '109.149359',
+  },
+  headers: {
+    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+    'X-RapidAPI-Key': '5ca8a0137cmshc18f81acd4fe5d5p1c075ejsne76a29a7fc52'
+  }
+};
+
+export const getPlacesData = async () => {
+  try { 
+    
+    const {data: {data}} = await axios.get(url, options);
+    return data;
+  } catch (error) {
+    console.log(`Fetch data Error: ${error}`);
+  }
+}
+
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,7 +4,9 @@ import List from "../components/List";
 import Map from "../components/Map";
 import PlaceDetail from "../components/PlaceDetail";
 import { useEffect, useState } from "react";
+import { getPlacesData } from "./api";
 
+// dummy data 
 const places = [
   { name: "sample Place1" },
   { name: "sample Place1" },
@@ -13,21 +15,29 @@ const places = [
 ];
 
 const Home = () => {
+
   //The 3 states below are for our Header component
   const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 });
   const [type, setType] = useState("restaurants");
   const [ratings, setRatings] = useState("");
+
   //This state is for talking to the API
   const [isLoading, setIsLoading] = useState(false);
 
+  // get the users current location on intial login 
   useEffect(() => {
-    // get the users current location on intial login 
-
     navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}}) => {
     console.log({latitude, longitude});
+    setCoordinates({lat: latitude, lng: longitude})
     })
 
   }, [])
+
+  useEffect(() => {
+    getPlacesData().then((data) => {
+      console.log(data);
+    })
+  } ,[])
 
   return (
     <Flex
