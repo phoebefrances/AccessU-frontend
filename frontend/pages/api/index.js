cacf02940edf27ab32b8051f1e03241099a55a38
Phoebe Windham <phoebe.frances@outlook.com>
2022-07-30 15:52:27 +0100

    dynamically coded the fetch request to take in the users choice of category rather than hard coding the url for restaurants, hotels etc

--- frontend/pages/api/index.js
+++ frontend/pages/api/index.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 
-const url =
-  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
 
-export const getPlacesData = async (sw, ne) => {
+export const getPlacesData = async (type, sw, ne) => {
   try {
     const {
       data: { data },
     } = await axios.get(
-      url, 
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, 
       {
         params: {
           bl_latitude: sw.lat,
