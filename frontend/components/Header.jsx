c262a913734b1fba989599348802b25cc47594ac
Phoebe Windham <phoebe.frances@outlook.com>
2022-07-31 11:26:52 +0100

    added onLoad and onPlaceChanged event to Autocomplete component, added state for autocomplete and onLoad method to save autocomplete information and onPlaceChanged method to get the latitude and longitude from the autocomplete using getPlace method

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -23,6 +23,17 @@ import {
 } from "react-icons/bi";
 
 const Header = ({ setType, setRatings, setCoordinates }) => {
+  
+  const [autocomplete, setAutocomplete] = useState(null);
+
+  const onLoad = (autoC) => setAutocomplete(autoC);
+
+  const onPlaceChanged = () => {
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+    setCoordinates({ lat, lng });
+  };
+
   return (
     <Flex
       position={"absolute"}
@@ -34,26 +45,26 @@ const Header = ({ setType, setRatings, setCoordinates }) => {
       zIndex={101}
     >
       <Flex>
-        {/* <Autocomplete> */}
-        <InputGroup width={"35vw"} shadow="lg">
-          <InputRightElement
-            pointerEvents={"none"}
-            children={<BiSearch color="gray" fontsize={20} />}
-          />
+        <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+          <InputGroup width={"35vw"} shadow="lg">
+            <InputRightElement
+              pointerEvents={"none"}
+              children={<BiSearch color="gray" fontSize={20} />}
+            />
 
-          <Input
-            type={"text"}
-            placeholder="Search Google Map..."
-            variant={"filled"}
-            fontSize={18}
-            bg={"white"}
-            color={"gray.700"}
-            _hover={{ bg: "whiteAlpha.800" }}
-            _focus={{ bg: "whiteAlpha.800" }}
-            _placeholder={{ color: "gray.700" }}
-          />
-        </InputGroup>
-        {/* </Autocomplete> */}
+            <Input
+              type={"text"}
+              placeholder="Search Google Map..."
+              variant={"filled"}
+              fontSize={18}
+              bg={"white"}
+              color={"gray.700"}
+              _hover={{ bg: "whiteAlpha.800" }}
+              _focus={{ bg: "whiteAlpha.800" }}
+              _placeholder={{ color: "gray.700" }}
+            />
+          </InputGroup>
+        </Autocomplete>
 
         <Flex alignItems={"center"} justifyContent={"center"}>
           <Flex
@@ -66,9 +77,9 @@ const Header = ({ setType, setRatings, setCoordinates }) => {
             ml={4}
             shadow="lg"
             cursor={"pointer"}
-            _hover={{ bg: 'gray.100'}}
-            transition= {'ease-in-out'}
-            transitionDuration = {'0.3s'}
+            _hover={{ bg: "gray.100" }}
+            transition={"ease-in-out"}
+            transitionDuration={"0.3s"}
           >
             <Menu>
               <BiStar fontSize={25} />
@@ -83,7 +94,7 @@ const Header = ({ setType, setRatings, setCoordinates }) => {
                   justifyContent="space-around"
                   onClick={() => setRatings("")}
                 >
-                  <Text fontsize={20} fontWeight={500} color={"orange.500"}>
+                  <Text fontSize={20} fontWeight={500} color={"gray.700"}>
                     All Rating
                   </Text>
                 </MenuItem>
@@ -92,9 +103,9 @@ const Header = ({ setType, setRatings, setCoordinates }) => {
                   display={"flex"}
                   alignItems={"center"}
                   justifyContent="space-around"
-                  onClick={() => setRatings("2")}
+                  onClick={() => setRatings(2)}
                 >
-                  <Text fontsize={20} fontWeight={500} color={"orange.500"}>
+                  <Text fontSize={20} fontWeight={500} color={"orange.500"}>
                     2.0
                   </Text>
 
@@ -105,9 +116,9 @@ const Header = ({ setType, setRatings, setCoordinates }) => {
                   display={"flex"}
                   alignItems={"center"}
                   justifyContent="space-around"
-                  onClick={() => setRatings("3")}
+                  onClick={() => setRatings(3)}
                 >
-                  <Text fontsize={20} fontWeight={500} color={"orange.500"}>
+                  <Text fontSize={20} fontWeight={500} color={"orange.500"}>
                     3.0
                   </Text>
 
@@ -118,9 +129,9 @@ const Header = ({ setType, setRatings, setCoordinates }) => {
                   display={"flex"}
                   alignItems={"center"}
                   justifyContent="space-around"
-                  onClick={() => setRatings("4")}
+                  onClick={() => setRatings(4)}
                 >
-                  <Text fontsize={20} fontWeight={500} color={"orange.500"}>
+                  <Text fontSize={20} fontWeight={500} color={"orange.500"}>
                     4.0
                   </Text>
 
@@ -131,23 +142,22 @@ const Header = ({ setType, setRatings, setCoordinates }) => {
                   display={"flex"}
                   alignItems={"center"}
                   justifyContent="space-around"
-                  onClick={() => setRatings("5")}
+                  onClick={() => setRatings(5)}
                 >
-                  <Text fontsize={20} fontWeight={500} color={"orange.500"}>
-                    5.0
+                  <Text fontSize={20} fontWeight={500} color={"orange.500"}>
+                    4.5
                   </Text>
 
                   <Rating size="small" value={5} readOnly />
                 </MenuItem>
               </MenuList>
             </Menu>
-
-            <BiChevronDown fontsize={25}/>
+            <BiChevronDown fontSize={25} />
           </Flex>
 
-          {/* {Restaurants} */}
+          {/* Restaurants */}
           <Flex
-           alignItems={"center"}
+            alignItems={"center"}
             justifyContent={"center"}
             px={4}
             py={2}
@@ -156,22 +166,20 @@ const Header = ({ setType, setRatings, setCoordinates }) => {
             ml={4}
             shadow="lg"
             cursor={"pointer"}
-            _hover={{ bg: 'gray.100'}}
-            transition= {'ease-in-out'}
-            transitionDuration = {'0.3s'}
-            onClick={() => setType('restaurants')}
+            _hover={{ bg: "gray.100" }}
+            transition={"ease-in-out"}
+            transitionDuration={"0.3s"}
+            onClick={() => setType("restaurants")}
           >
-            <BiRestaurant fontsize={25}/>
-            <Text ml={3} fontsize={16} fontWeight={500}>
+            <BiRestaurant fontSize={25} />
+            <Text ml={3} fontSize={16} fontWeight={500}>
               Restaurants
             </Text>
           </Flex>
 
-
-
-          {/* {Hotels} */}
+          {/* Hotels */}
           <Flex
-           alignItems={"center"}
+            alignItems={"center"}
             justifyContent={"center"}
             px={4}
             py={2}
@@ -180,22 +188,20 @@ const Header = ({ setType, setRatings, setCoordinates }) => {
             ml={4}
             shadow="lg"
             cursor={"pointer"}
-            _hover={{ bg: 'gray.100'}}
-            transition= {'ease-in-out'}
-            transitionDuration = {'0.3s'}
-            onClick={() => setType('hotels')}
+            _hover={{ bg: "gray.100" }}
+            transition={"ease-in-out"}
+            transitionDuration={"0.3s"}
+            onClick={() => setType("hotels")}
           >
-            <BiHotel fontsize={25}/>
-            <Text ml={3} fontsize={16} fontWeight={500}>
+            <BiHotel fontSize={25} />
+            <Text ml={3} fontSize={16} fontWeight={500}>
               Hotels
             </Text>
           </Flex>
 
-
-
-          {/* {Atrractions} */}
+          {/* Attractions */}
           <Flex
-           alignItems={"center"}
+            alignItems={"center"}
             justifyContent={"center"}
             px={4}
             py={2}
@@ -204,13 +210,13 @@ const Header = ({ setType, setRatings, setCoordinates }) => {
             ml={4}
             shadow="lg"
             cursor={"pointer"}
-            _hover={{ bg: 'gray.100'}}
-            transition= {'ease-in-out'}
-            transitionDuration = {'0.3s'}
-            onClick={() => setType('attractions')}
+            _hover={{ bg: "gray.100" }}
+            transition={"ease-in-out"}
+            transitionDuration={"0.3s"}
+            onClick={() => setType("attractions")}
           >
-            <BiMapAlt fontsize={25}/>
-            <Text ml={3} fontsize={16} fontWeight={500}>
+            <BiMapAlt fontSize={25} />
+            <Text ml={3} fontSize={16} fontWeight={500}>
               Attractions
             </Text>
           </Flex>
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,63 +1,56 @@
 import { Flex } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import List from "../components/List";
 import Map from "../components/Map";
 import PlaceDetail from "../components/PlaceDetail";
-import { useEffect, useState } from "react";
 import { getPlacesData } from "./api/getPlacesData";
+import Head from "next/head";
 
 // dummy data 
-// const places = [
-//   { name: "sample Place1" },
-//   { name: "sample Place1" },
-//   { name: "sample Place1" },
-//   { name: "sample Place1" },
-// ];
+const places = [
+  { name: "sample Place1" },
+  { name: "sample Place1" },
+  { name: "sample Place1" },
+  { name: "sample Place1" },
+];
 
 const Home = () => {
-
-  //The 3 states below are for our Header component
-  const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 });
+  const [places, setPlaces] = useState([]);
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState(null);
   const [type, setType] = useState("restaurants");
   const [ratings, setRatings] = useState("");
-
-  //This state is for talking to the API
   const [isLoading, setIsLoading] = useState(false);
 
-  // This state is for getting the lat and lng for ne, nw, se, sw boundaries
-  const [bounds, setBounds] = useState(null)
-
-  // This state is for updating the places once the places data has loaded which is passed to the List
-  const [places, setPlaces] = useState([])
-
-  // This state is for updating the data if the user chooses to filter the places listed e.g. by rating, n.b. filteredPlaces is passed to places
-  const [filteredPlaces, setFilteredPlaces] = useState([])
-
-  // get the users current location on intial login 
+// get the users current location on intial login
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}}) => {
-    console.log({latitude, longitude});
-    setCoordinates({lat: latitude, lng: longitude})
-    })
 
-  }, [])
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        console.log({ latitude, longitude });
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
 
-  // updates the data to the users choice of rating 
+// updates the data to the users choice of rating 
   useEffect(() => {
     const filteredData = places.filter((place) => place.rating > ratings);
     setFilteredPlaces(filteredData);
     console.log({ ratings });
   }, [ratings]);
-  
+
   // updates the data to the users choice of category or location 
   useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
     getPlacesData(type, bounds?.sw, bounds?.ne).then((data) => {
       console.log(data);
-      setPlaces(data)
-      setIsLoading(false)
-    })
-  } ,[type, coordinates, bounds])
+      setPlaces(data);
+      setIsLoading(false);
+    });
+  }, [type, coordinates, bounds]);
 
   return (
     <Flex
@@ -69,21 +62,27 @@ const Home = () => {
       maxHeight={"100vh"}
       position={"relative"}
     >
+      <Head>
+        <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyD93tjfea30qHGkuhHJWQ0vQB9FF-HYIZo"></script>
+      </Head>
+
       <Header
         setType={setType}
         setRatings={setRatings}
         setCoordinates={setCoordinates}
       />
 
-      <List 
-      places={filteredPlaces.length ? filteredPlaces : places}
-      isLoading={isLoading} />
+      <List
+        places={filteredPlaces.length ? filteredPlaces : places}
+        isLoading={isLoading}
+      />
 
-      <Map 
-      setCoordinates={setCoordinates}
-      coordinates={coordinates} 
-      setBounds={setBounds}
-      places ={filteredPlaces.length ? filteredPlaces : places} />
+      <Map
+        setCoordinates={setCoordinates}
+        coordinates={coordinates}
+        setBounds={setBounds}
+        places={filteredPlaces.length ? filteredPlaces : places}
+      />
     </Flex>
   );
 };
