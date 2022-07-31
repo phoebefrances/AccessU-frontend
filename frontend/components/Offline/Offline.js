949a26efa68eb389a0e714f36cab950a635379e7
enantes <sambaeverything@gmail.com>
2022-08-01 00:07:18 +0200

    chakra UI component

--- frontend/components/Offline/Offline.js
+++ frontend/components/Offline/Offline.js
@@ -39,3 +39,70 @@ const Map = ({ coordinates, setCoordinates, setBounds, places }) => {
             <IoLocation color="red" fontSize={30} />
           </Box>
         ))}
+
+          {/* Conditionally rendered the Box chakra component if isCard is true  */}
+                            {isCard && (
+                            <Box
+                                width={"200px"}
+                                height={"150px"}
+                                bg={"whiteAlpha.900"}
+                                position={"absolute"}
+                                top={-12}
+                                left={0}
+                                shadow={"lg"}
+                                rounded={"lg"}
+                            >
+
+                            {/* Chakra Image component to display place Image on small card */}
+                                <Image
+                                objectFit={"cover"}
+                                width={"full"}
+                                height={"120px"}
+                                rounded="lg"
+                                src={
+                                    cardData?.photo
+                                    ? cardData?.photo?.images?.large?.url
+                                    : "https://explorelompoc.com/wp-content/uploads/2021/06/food_placeholder.jpg"
+                                }
+                                />
+                                
+                                {/* Chakra Text component to display place name details on small card */}
+                                <Text
+                                textTransform={"capitalize"}
+                                width={"40"}
+                                fontSize={"lg"}
+                                fontWeight={"500"}
+                                isTruncated
+                                >
+                                {cardData.name}
+                                </Text>
+
+                                <Box
+                                cursor={"pointer"}
+                                position={"absolute"}
+                                top={2}
+                                right={2}
+                                width={"30px"}
+                                height={"30px"}
+                                bg={"red.300"}
+                                rounded={"full"}
+                                display={"flex"}
+                                justifyContent={"center"}
+                                alignItems={"center"}
+                                onClick={() => {
+                                    setIsCard(false);
+                                }}
+                                >
+                                <BiX fontSize={20} />
+                                </Box>
+                            </Box>
+                            )}
+                        </GoogleMapReact>
+                        </Box>
+                    );
+};
+
+export default Map;
+
+
+
