a4177d1650305757459b3cda71e70f38b78c2860
enantes <sambaeverything@gmail.com>
2022-08-01 00:05:18 +0200

    imports added

--- frontend/components/Offline/Offline.js
+++ frontend/components/Offline/Offline.js
@@ -1,2 +1,5 @@
 import React, { useState } from "react";
-
+import { Box, Image, Text } from "@chakra-ui/react";
+import GoogleMapReact from "google-map-react";
+import { IoLocation } from "react-icons/io5";
+import { BiX } from "react-icons/bi";
