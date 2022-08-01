461df3025dc4aec13921d18d793a3cae8da2ab1f
Phoebe Windham <phoebe.frances@outlook.com>
2022-08-01 15:52:46 +0100

    version 01/08

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -12,11 +12,12 @@ import {
   Button,
   Grid, 
   GridItem,
-  Divider
+  Divider, 
 } from "@chakra-ui/react";
 import { Rating } from "@material-ui/lab";
 import { Autocomplete } from "@react-google-maps/api";
 import React, { useState } from "react";
+import { Search2Icon } from '@chakra-ui/icons'
 
 import {
   BiChevronDown,
@@ -41,18 +42,16 @@ const Header = ({ setType, setRatings, setCoordinates }) => {
 
   return (
     <Flex
-      position={"absolute"}
-      top={0}
-      left={0}
-      width={"full"}
-      px={4}
-      py={2}
-      zIndex={101}
-      borderColor={"blue"}
-    >
+          position={"absolute"}
+          top={0}
+          left={0}
+          width={"full"}
+          px={4}
+          py={2}
+          zIndex={101}>
       <Flex>
-        <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}/>
-          <InputGroup width={"35vw"} shadow="lg"/>
+        <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+          <InputGroup width={"35vw"} shadow="lg">
             {/* <InputRightElement
               pointerEvents={"none"}
               children={<BiSearch color="gray" fontSize={20} />}
@@ -104,6 +103,8 @@ const Header = ({ setType, setRatings, setCoordinates }) => {
             </Button>
             </GridItem>
             </Grid>
+            </InputGroup>
+        </Autocomplete>
           
        
 
@@ -263,8 +264,10 @@ const Header = ({ setType, setRatings, setCoordinates }) => {
           </Flex>
           </Flex>
       </Flex>
-    </Flex>
+      </Flex> 
+ 
   );
 };
 
 export default Header;
+
