dc6df6857d2e38d4df145018097dc9adc12fc676
admin <admin@MacBook-Air.local>
2022-08-02 17:20:58 +0200

    Added setPlace state to dynamically control the place data

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -7,11 +7,11 @@ import PlaceDetail from "../components/PlaceDetail";
 import { getPlacesData } from "./api/getPlacesData";
 import Head from "next/head";
 //👇 Comment out if using API. Uncomment if using offline database.
-import { places } from "../libs/offlineData.js";
+import { placesData } from "../libs/offlineData.js";
 
 const Home = () => {
   //👇 Comment out if using offline database. Uncomment if using API
-  // const [places, setPlaces] = useState([]);
+const [places, setPlaces] = useState({});
   const [filteredPlaces, setFilteredPlaces] = useState([]);
   const [coordinates, setCoordinates] = useState({});
   const [bounds, setBounds] = useState(null);
@@ -19,8 +19,8 @@ const Home = () => {
    const [type, setType] = useState("restaurants");
   const [ratings, setRatings] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  setPlaces(placesData)
 
-  
   //👇 Comment out if using API. Uncomment if using offline database.
 
   // get the users current location on intial login
@@ -43,9 +43,14 @@ const Home = () => {
   useEffect(() => {
     const filteredCategory = places.filter((place) => place.category === category);
     // setCategory(filteredCategory)
+    setPlaces(filteredCategory)
     console.log(filteredCategory);
   }, [category]);
 
+  useEffect(() => {
+
+    //re-render map
+  }, [places]);
   // updates the data to the users choice of category or location
   // 👇 Comment out if using offline database. Uncomment if using the API
   // useEffect(() => {
