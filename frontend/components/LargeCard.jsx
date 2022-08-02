14289c5533743eda1ddb7193b0fed1708f386abe
shabana kauser <shabanakauser89@gmail.com>
2022-08-02 13:59:18 +0100

    added some styling to big card

--- frontend/components/LargeCard.jsx
+++ frontend/components/LargeCard.jsx
@@ -1,70 +1,73 @@
-import { Box, Image, Badge } from '@chakra-ui/react'
+import { Box, Image, Badge } from "@chakra-ui/react";
 import { Rating } from "@material-ui/lab";
 
-
 function LargeCard() {
-    const property = {
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'Rear view of modern home with pool',
-      beds: 3,
-      baths: 2,
-      title: 'Modern home in city center in the heart of historic Los Angeles',
-      formattedPrice: '$1,900.00',
-      reviewCount: 34,
-      rating: 4,
-    }
-  
-    return (
-      <Box  bg="white" position="absolute" width='217px' height='397px' left='324px' top='138px'>
-        <Image src={property.imageUrl} alt={property.imageAlt} />
-  
-        <Box p='6'>
-          <Box display='flex' alignItems='baseline'>
-            <Badge borderRadius='full' px='2' colorScheme='teal'>
-              New
-            </Badge>
-            <Box
-              color='gray.500'
-              fontWeight='semibold'
-              letterSpacing='wide'
-              fontSize='xs'
-              textTransform='uppercase'
-              ml='2'
-            >
-              {property.beds} beds &bull; {property.baths} baths
-            </Box>
-          </Box>
-  
+  const property = {
+    imageUrl: "https://bit.ly/2Z4KKcF",
+    imageAlt: "Rear view of modern home with pool",
+    title: "Cineworld",
+    category: "Cinema",
+    address: "55 Bluewater Road, Manchester, M25 6HB",
+    telephoneNumber: "02813849583",
+    website: "www.cineworld.com",
+    openingTime: "9:00 - 21:00",
+    rating: 4,
+  };
+
+  return (
+    <Box
+      bg="white"
+      position="absolute"
+      width="217px"
+      height="397px"
+      left="530px"
+      top="138px"
+    >
+      <Image src={property.imageUrl} alt={property.imageAlt} />
+
+      <Box p="6">
+        <Box display="flex" alignItems="baseline">
           <Box
-            mt='1'
-            fontWeight='semibold'
-            as='h4'
-            lineHeight='tight'
-            noOfLines={1}
-          >
-            {property.title}
-          </Box>
-  
-          <Box>
-            {property.formattedPrice}
-            <Box as='span' color='gray.600' fontSize='sm'>
-              / wk
-            </Box>
-          </Box>
-
-  
-          <Box display='flex' mt='2' alignItems='center'>
-          <Rating 
-            size="small" 
-            value={Number(property.rating)}
-            readOnly />
-            <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-              {property.reviewCount} reviews
-            </Box>
-          </Box>
+            color="gray.500"
+            fontWeight="semibold"
+            letterSpacing="wide"
+            fontSize="xs"
+            textTransform="uppercase"
+            ml="2"
+          ></Box>
         </Box>
-      </Box>
-    )
-  }
+        <Rating size="small" value={Number(property.rating)} readOnly />
+        <Box
+          mt="1"
+          fontWeight="semibold"
+          as="h4"
+          lineHeight="tight"
+          noOfLines={1}
+        >
+          {property.title}
+        </Box>
+        <Box
+          mt="1"
+          fontWeight="semibold"
+          as="h4"
+          lineHeight="tight"
+          noOfLines={1}
+        >
+          {property.category}
+          {property.address}
+          {property.telephoneNumber}
+          {property.website}
+          {property.openingTime}
+        </Box>
+        <Box>
+          {property.formattedPrice}
+          <Box as="span" color="gray.600" fontSize="sm"></Box>
+        </Box>
 
-  export default LargeCard;
+        <Box display="flex" mt="2" alignItems="center"></Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default LargeCard;
