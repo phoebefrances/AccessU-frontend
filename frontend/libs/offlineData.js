726c90f71c41d7e6fca8dba1415851c79aa8404d
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-08-02 10:16:26 +0100

    Added 5 vanues to the offline database. Added category value to offline database. Added accesibility tokens

--- frontend/libs/offlineData.js
+++ frontend/libs/offlineData.js
@@ -2,7 +2,8 @@
 
 export let data = [
   {
-    name: "Your mum's house",
+    category: "restaurant",
+    name: "The Breakfast Club Soho",
     price: "$5 - $20",
     rating: "5.0",
     num_reviews: "69",
@@ -20,8 +21,105 @@ export let data = [
       },
     },
     address: "18 Castle Street, Buckingham MK18 1BP England",
-    longitude: "-0.988513",
-    latitude: "51.999294",
+    longitude: "-0.13521278619092814",
+    latitude: "51.51226090514994",
+    accessibility_badges: ["step_free", "braille", "sign_lang", "quiet"],
+  },
+  {
+    category: "cinema",
+    name: "Prince Charles Cinema",
+    price: "$5 - $20",
+    rating: "4.0",
+    num_reviews: "69",
+    price_level: "$$ - $$$",
+    ranking: "no 1 traphouse in da hood",
+    open_now_text: "open now",
+    dietary_restrictions: [{ key: "10665", name: "Vegetarian Friendly" }],
+    photo: {
+      images: {
+        large: {
+          width: "828",
+          url: "https://media-cdn.tripadvisor.com/media/photo-o/1a/4c/1f/f8/wagyu-japones.jpg",
+          height: "461",
+        },
+      },
+    },
+    address: "18 Castle Street, Buckingham MK18 1BP England",
+    longitude: "-0.13042340946513464",
+    latitude: "51.51184071143942",
+    accessibility_badges: ["step_free", "braille", "sign_lang", "quiet"],
+  },
+  {
+    category: "shop",
+    name: "Big Sainsburys",
+    price: "$5 - $20",
+    rating: "3.0",
+    num_reviews: "69",
+    price_level: "$$ - $$$",
+    ranking: "no 1 traphouse in da hood",
+    open_now_text: "open now",
+    dietary_restrictions: [{ key: "10665", name: "Vegetarian Friendly" }],
+    photo: {
+      images: {
+        large: {
+          width: "828",
+          url: "https://media-cdn.tripadvisor.com/media/photo-o/1a/4c/1f/f8/wagyu-japones.jpg",
+          height: "461",
+        },
+      },
+    },
+    address: "18 Castle Street, Buckingham MK18 1BP England",
+    longitude: "-0.1386631551988991",
+    latitude: "51.51787642984117",
+    accessibility_badges: ["step_free", "braille", "sign_lang", "quiet"],
+  },
+  {
+    category: "gym",
+    name: "Alpha Lobster Gym",
+    price: "$5 - $20",
+    rating: "3.0",
+    num_reviews: "69",
+    price_level: "$$ - $$$",
+    ranking: "no 1 traphouse in da hood",
+    open_now_text: "open now",
+    dietary_restrictions: [{ key: "10665", name: "Vegetarian Friendly" }],
+    photo: {
+      images: {
+        large: {
+          width: "828",
+          url: "https://media-cdn.tripadvisor.com/media/photo-o/1a/4c/1f/f8/wagyu-japones.jpg",
+          height: "461",
+        },
+      },
+    },
+    address: "18 Castle Street, Buckingham MK18 1BP England",
+    longitude: "-0.13119588562767506",
+    latitude: "51.51683492491375",
+    accessibility_badges: ["step_free", "braille", "sign_lang", "quiet"],
+  },
+  {
+    category: "café",
+    name: "Roasted Beanz",
+    price: "$5 - $20",
+    rating: "3.0",
+    num_reviews: "69",
+    price_level: "$ - $$",
+    ranking: "no 1 traphouse in da hood",
+    open_now_text: "open now",
+    dietary_restrictions: [{ key: "10665", name: "Vegetarian Friendly" }],
+    photo: {
+      images: {
+        large: {
+          width: "828",
+          url: "https://media-cdn.tripadvisor.com/media/photo-o/1a/4c/1f/f8/wagyu-japones.jpg",
+          height: "461",
+        },
+      },
+    },
+    address: "18 Castle Street, Buckingham MK18 1BP England",
+    longitude: "-0.1381910864329022",
+    latitude: "51.50871571116749",
+    accessibility_badges: ["step_free", "braille", "sign_lang", "quiet"],
   },
 ];
 
