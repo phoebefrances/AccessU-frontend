e6c8b147741b41dfa97e924f8f0b406d275a6457
kd071090 <kierandickerson3@gmail.com>
2022-08-03 12:15:26 +0100

    added a close button and styling to large card

--- frontend/components/LargeCard.jsx
+++ frontend/components/LargeCard.jsx
@@ -4,6 +4,7 @@ import SignLanguageIcon from "@mui/icons-material/SignLanguage";
 import HearingIcon from "@mui/icons-material/Hearing";
 import AccessibleIcon from "@mui/icons-material/Accessible";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import { CloseButton } from '@chakra-ui/react'
 
 function LargeCard() {
   const property = {
@@ -30,8 +31,10 @@ function LargeCard() {
       height="470px"
       left="530px"
       top="138px"
-    >
-      <Image rounded="lg" src={property.imageUrl} alt={property.imageAlt} />
+      >
+  
+    
+      <Image rounded="lg" src={property.imageUrl} alt={property.imageAlt} position= 'relative' />
       <Box p="3">
         <Box display="flex" alignItems="baseline">
           <Box
@@ -41,8 +44,20 @@ function LargeCard() {
             fontSize="xs"
             textTransform="uppercase"
             ml="2"
+            position= 'relative'
           ></Box>
         </Box>
+        <Box direction='row' spacing={6}>
+  <CloseButton size='sm' position= 'absolute'
+ bg={`#FF9100`}
+ colorScheme='white'
+ color='white'
+ borderColor='white'
+  // z-index= '20'
+  right= '1' 
+  top= '1'
+  onClick= 'onClose' />
+</Box>
         <Box
           mt="1"
           fontWeight="semibold"
@@ -123,5 +138,46 @@ function LargeCard() {
     </Box>
   );
 }
+
+
+function SizeExample() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [size, setSize] = React.useState('md')
+
+  const handleSizeClick = (newSize) => {
+    setSize(newSize)
+    onOpen()
+  }
+
+  const sizes = ['xs']
+
+  return (
+    <>
+      {sizes.map((size) => (
+        <Button
+          onClick={() => handleSizeClick(size)}
+          key={size}
+          m={4}
+        >{`Open ${size} Modal`}</Button>
+      ))}
+
+      <Modal onClose={onClose} size={size} isOpen={isOpen}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Lorem count={2} />
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
+
 
 export default LargeCard;
