ce149a6fceb1dd700fbe5d87c668d387431ad831
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-08-04 11:27:11 +0100

    cleaned up the small card- deleted surplus flexboxes, added one extra flexbox

--- frontend/components/PlaceDetail.jsx
+++ frontend/components/PlaceDetail.jsx
@@ -5,10 +5,8 @@ import { IoLocation } from "react-icons/io5";
 import SignLanguageIcon from "@mui/icons-material/SignLanguage";
 import HearingIcon from "@mui/icons-material/Hearing";
 import AccessibleIcon from "@mui/icons-material/Accessible";
-import PsychologyIcon from '@mui/icons-material/Psychology';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-
-
+import PsychologyIcon from "@mui/icons-material/Psychology";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 
 const PlaceDetail = ({ place }) => {
   return (
@@ -21,51 +19,128 @@ const PlaceDetail = ({ place }) => {
       direction={"column"}
       alignItems={"start"}
       justifyContent="space-between"
+      bg="red"
+      direction="row"
     >
-      <Flex justifyContent={"space-between"} width="full">
+      <Flex direction="column" bg="yellow">
         <Flex
-          direction={"column"}
-          justifyContent={"start"}
-          alignItems={"start"}
-          width="full"
-          px={2}
+          alignItems={"center"}
+          width={"full"}
+          justifyContent={"space-between"}
+          bg="purple"
         >
-          <Flex
-            alignItems={"center"}
-            width={"full"}
-            justifyContent={"space-between"}
+          <Text
+            textTransform={"capitalize"}
+            width={"40"}
+            fontSize={"lg"}
+            fontWeight={"500"}
           >
-            <Text
+            {place.name}
+          </Text>
+        </Flex>
+
+        {/* Ratings */}
+        <Flex alignItems={"center"} width={"full"} bg="blue">
+          <Rating size="small" value={Number(place.rating)} readOnly />
+        </Flex>
+
+        {place?.address && (
+          <Flex alignItems={"center"} width={"full"} px={1} my={2} bg="orange">
+            <IoLocation fontSize={20} color="gray" />
+
+            <Text fontSize={"small"} fontWeight={500} color={"gray.700"} ml={1}>
+              {place.address}
+            </Text>
+          </Flex>
+        )}
+
+        <Flex bg="pink" width="full">
+          {place.acessibility?.accessible && (
+            <Flex alignItems={"center"} width={"full"} px={1} my={2}>
+              <Text
+                fontSize={"small"}
+                fontWeight={500}
+                color={"gray.700"}
+                ml={1}
+              >
+                <AccessibleIcon fontSize="large" />
+              </Text>
+            </Flex>
+          )}
+          {place.acessibility?.eye && (
+            <Flex alignItems={"center"} width={"full"} px={1} my={2}>
+              <Text
+                fontSize={"small"}
+                fontWeight={500}
+                color={"gray.700"}
+                ml={1}
+              >
+                <VisibilityIcon fontSize="large" />
+              </Text>
+            </Flex>
+          )}
+          {place.acessibility?.hearing && (
+            <Flex alignItems={"center"} width={"full"} px={1} my={2}>
+              <Text
+                fontSize={"small"}
+                fontWeight={500}
+                color={"gray.700"}
+                ml={1}
+              >
+                <HearingIcon fontSize="large" />
+              </Text>
+            </Flex>
+          )}
+          {place.acessibility?.brain && (
+            <Flex alignItems={"center"} width={"full"} px={1} my={2}>
+              <Text
+                fontSize={"small"}
+                fontWeight={500}
+                color={"gray.700"}
+                ml={1}
+              >
+                <PsychologyIcon fontSize="large" />
+              </Text>
+            </Flex>
+          )}
+        </Flex>
+      </Flex>
+      <Image
+        objectFit={"cover"}
+        height={"120px"}
+        width={"120px"}
+        rounded="lg"
+        bg="green"
+        src={
+          place.photo
+            ? place.photo
+            : "https://explorelompoc.com/wp-content/uploads/2021/06/food_placeholder.jpg"
+        }
+      />
+    </Flex>
+  );
+};
+
+export default PlaceDetail;
+
+{
+  /* <Text
               textTransform={"capitalize"}
               width={"40"}
               fontSize={"lg"}
               fontWeight={"500"}
-            >
-              {place.name}
-            </Text>
+              bgColor="blue"
+            ></Text> */
+}
 
-            <Text
-              textTransform={"capitalize"}
-              width={"40"}
-              fontSize={"lg"}
-              fontWeight={"500"}
-            >
-              
-
-              
-            </Text>
-
-
-          </Flex>
-
-          {/* Ratings */}
-          <Flex alignItems={"center"} width={"full"}>
-            <Rating size="small" value={Number(place.rating)} readOnly />
-          </Flex>
-
-          {/* dietary_restrictions */}
-          {/* We may use this as behaviour for rendering accesibility badges */}
-          {/* {place?.dietary_restrictions && (
+{
+  /* dietary_restrictions */
+}
+{
+  /* We may use this as behaviour for rendering accesibility badges */
+}
+{
+  /* {place?.dietary_restrictions && (
             <Flex width={"full"} flexWrap={"wrap"}>
               {place.dietary_restrictions.map((n, i) => (
                 <Badge
@@ -79,73 +154,5 @@ const PlaceDetail = ({ place }) => {
                 </Badge>
               ))}
             </Flex>
-          )} */}
-        </Flex>
-      </Flex>
-
-      <Image
-        objectFit={"cover"}
-        width={"120px"}
-        height={"120px"}
-        rounded="lg"
-        src={
-          place.photo
-            ? place.photo
-            : "https://explorelompoc.com/wp-content/uploads/2021/06/food_placeholder.jpg"
-        }
-      />
-
-      {place?.address && (
-        <Flex alignItems={"center"} width={"full"} px={1} my={2}>
-          <IoLocation fontSize={20} color="gray" />
-
-          <Text fontSize={"small"} fontWeight={500} color={"gray.700"} ml={1}>
-            {place.address}
-          </Text>
-        </Flex>
-      )}
-
-      <Flex>
-      {place.acessibility?.accessible && (
-        <Flex alignItems={"center"} width={"full"} px={1} my={2}>
-
-          <Text fontSize={"small"} fontWeight={500} color={"gray.700"} ml={1}>
-          
-            <AccessibleIcon fontSize="large" />
-          </Text>
-        </Flex>
-
-      )}
-      {place.acessibility?.eye && (
-        <Flex alignItems={"center"} width={"full"} px={1} my={2}>
-
-          <Text fontSize={"small"} fontWeight={500} color={"gray.700"} ml={1}>
-          
-            <VisibilityIcon fontSize="large" />
-          </Text>
-        </Flex>
-      )}
-      {place.acessibility?.hearing && (
-        <Flex alignItems={"center"} width={"full"} px={1} my={2}>
-
-          <Text fontSize={"small"} fontWeight={500} color={"gray.700"} ml={1}>
-          
-            <HearingIcon fontSize="large" />
-          </Text>
-        </Flex>
-      )}
-      {place.acessibility?.brain && (
-        <Flex alignItems={"center"} width={"full"} px={1} my={2}>
-
-          <Text fontSize={"small"} fontWeight={500} color={"gray.700"} ml={1}>
-          
-            <PsychologyIcon fontSize="large" />
-          </Text>
-        </Flex>
-      )}
-      </Flex> 
-    </Flex>
-  );
-};
-
-export default PlaceDetail;
+          )} */
+}
