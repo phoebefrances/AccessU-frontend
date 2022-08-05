ded444dde6a206f3f15bee56b4a93ca66bbb0816
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-08-05 10:32:11 +0100

    deleted the red X and replaced with functionality which kills the card once map is clicked; updated the to make them match those in small card; repositioned the big card; added descriptions to the accessibility icons

--- frontend/components/Map.jsx
+++ frontend/components/Map.jsx
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
-import { Box, Image, Spacer, Text } from "@chakra-ui/react";
+import { Box, Image, Spacer, Text, Flex } from "@chakra-ui/react";
 import GoogleMapReact from "google-map-react";
 import { IoLocation } from "react-icons/io5";
-import { BiX } from "react-icons/bi";
-import places from "../libs/offlineData.js"
 import { Rating } from "@material-ui/lab";
-import SignLanguageIcon from "@mui/icons-material/SignLanguage";
-import HearingIcon from "@mui/icons-material/Hearing";
+
+//Icon imports:
 import AccessibleIcon from "@mui/icons-material/Accessible";
-import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import HearingIcon from "@mui/icons-material/Hearing";
+import PsychologyIcon from "@mui/icons-material/Psychology";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 
-
-const Map = ({ coordinates, setCoordinates, setBounds, places, place}) => {
-   //This state updates when you click the Iolocation icon for a particular place, that place icon will update from not having a card to having a card i.e. from false to true 
+const Map = ({ coordinates, setCoordinates, setBounds, places, place }) => {
+  //This state updates when you click the Iolocation icon for a particular place, that place icon will update from not having a card to having a card i.e. from false to true
   const [isCard, setIsCard] = useState(false);
 
   //This state updates the card for the icon of a particular place from not having any details to having details i.e. from null to '*Whatever details are fetched*'
@@ -21,7 +20,7 @@ const Map = ({ coordinates, setCoordinates, setBounds, places, place}) => {
   return (
     <Box width={"full"} height={"full"}>
       <GoogleMapReact
-        bootstrapURLKeys={{key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}}
+        bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY }}
         // icon={url: 'http://maps.google.com/mapfiles/kml/paddle/blu-blank.png'}
         defaultCenter={coordinates}
         center={coordinates}
@@ -32,10 +31,14 @@ const Map = ({ coordinates, setCoordinates, setBounds, places, place}) => {
           setCoordinates({ lat: e.center.lat, lng: e.center.lng });
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
-        //added click event by setting setCardData to places and passed the child as an array to places to get the index number (not getting an object) and set setisCard to true 
+        //added click event by setting setCardData to places and passed the child as an array to places to get the index number (not getting an object) and set setisCard to true
         onChildClick={(child) => {
           setCardData(places[child]);
           setIsCard(true);
+        }}
+        //This kills the big card when the user clicks away from it (i.e. anywhere on the map)
+        onClick={() => {
+          setIsCard(false);
         }}
       >
         {places?.map((place, i) => (
@@ -49,22 +52,18 @@ const Map = ({ coordinates, setCoordinates, setBounds, places, place}) => {
           </Box>
         ))}
 
-          {/* Conditionally rendered the Box chakra component if isCard is true  */}
+        {/* Conditionally rendered the Box chakra component if isCard is true  */}
         {isCard && (
           <Box
-            width={"200px"}
-            height={"500px"}
-            bg={"whiteAlpha.900"}
-            position={"relative"}
-            top={-20}
-            left={0}
-            shadow={"lg"}
-            rounded={"lg"}
-            
+            bg="white"
+            height="500px"
+            width="250px"
+            position={"absolute"}
+            top={"-25vh"}
+            left={"-25vw"}
+            bottom="50px"
           >
-          
-
-           {/* Chakra Image component to display place Image on small card */}
+            {/* Chakra Image component to display place Image on small card */}
             <Image
               objectFit={"cover"}
               width={"full"}
@@ -72,9 +71,9 @@ const Map = ({ coordinates, setCoordinates, setBounds, places, place}) => {
               rounded="lg"
               src={cardData.photo}
             />
-            
+
             {/* Chakra Text component to display place name details on small card */}
-            
+
             <Text
               textTransform={"capitalize"}
               width={"40"}
@@ -85,8 +84,8 @@ const Map = ({ coordinates, setCoordinates, setBounds, places, place}) => {
               color={"#2C2C68"}
             >
               {cardData.category}
-              </Text>
-              <Text
+            </Text>
+            <Text
               textTransform={"capitalize"}
               width={"40"}
               height={"20px"}
@@ -96,8 +95,8 @@ const Map = ({ coordinates, setCoordinates, setBounds, places, place}) => {
               color={"#2C2C68"}
             >
               {cardData.name}
-              </Text>
-              <Text
+            </Text>
+            <Text
               textTransform={"capitalize"}
               width={"40"}
               height={"20px"}
@@ -106,10 +105,10 @@ const Map = ({ coordinates, setCoordinates, setBounds, places, place}) => {
               isTruncated
               color={"#2C2C68"}
             >
-             <Rating size="small" value={Number(cardData.rating)} readOnly />
-              </Text>
-              <Spacer />
-              <Text
+              <Rating size="small" value={Number(cardData.rating)} readOnly />
+            </Text>
+            <Spacer />
+            <Text
               textTransform={"capitalize"}
               width={"40"}
               height={"20px"}
@@ -119,7 +118,6 @@ const Map = ({ coordinates, setCoordinates, setBounds, places, place}) => {
               color={"#2C2C68"}
             >
               {cardData.address}
-
             </Text>
             <br></br>
             <Text
@@ -132,7 +130,6 @@ const Map = ({ coordinates, setCoordinates, setBounds, places, place}) => {
               color={"#2C2C68"}
             >
               {cardData.phone_number}
-
             </Text>
             <Spacer />
             <br></br>
@@ -146,7 +143,6 @@ const Map = ({ coordinates, setCoordinates, setBounds, places, place}) => {
               color={"#2C2C68"}
             >
               {cardData.web_address}
-
             </Text>
             <br></br>
             <Text
@@ -159,31 +155,88 @@ const Map = ({ coordinates, setCoordinates, setBounds, places, place}) => {
               color={"#2C2C68"}
             >
               {cardData.opening_times}
-
             </Text>
             <br></br>
-            <SignLanguageIcon color={"#032396"} fontSize="large"/>
-            <HearingIcon fontSize="large" />
-            <RemoveRedEyeIcon fontSize="large" />
-            <AccessibleIcon fontSize="large" />
-            <Box
-              cursor={"pointer"}
-              position={"absolute"}
-              top={2}
-              right={2}
-              width={"30px"}
-              height={"30px"}
-              bg={"red.300"}
-              rounded={"full"}
-              display={"flex"}
-              justifyContent={"center"}
-              alignItems={"center"}
-              onClick={() => {
-                setIsCard(false);
-              }}
+
+            <Flex
+              width="full"
+              // bg="red"
+              direction="column"
+              gap="3"
+              pt={"3"}
             >
-              <BiX fontSize={20} />
-            </Box>
+              {cardData.acessibility?.accessible && (
+                <Flex
+                  alignItems={"center"}
+                  width={"full"}
+                  px={1}
+                  height={"25%"}
+                  // bg="yellow"
+                >
+                  <AccessibleIcon fontSize="large" color={"#2C2C68"} />
+                  <Text fontSize={"small"} fontWeight={500} color={"#2C2C68"}>
+                    This place is wheelchair accesible
+                  </Text>
+                </Flex>
+              )}
+              {cardData.acessibility?.eye && (
+                <Flex
+                  alignItems={"center"}
+                  width={"full"}
+                  px={1}
+                  height={"25%"}
+                  // bg="yellow"
+                >
+                  <VisibilityIcon fontSize="large" color={"#2C2C68"} />
+                  <Text
+                    fontSize={"small"}
+                    fontWeight={500}
+                    color={"#2C2C68"}
+                    ml={1}
+                  >
+                    This place has adjustments for visual impairments
+                  </Text>
+                </Flex>
+              )}
+              {cardData.acessibility?.hearing && (
+                <Flex
+                  alignItems={"center"}
+                  width={"full"}
+                  px={1}
+                  height={"25%"}
+                  // bg="yellow"
+                >
+                  <HearingIcon fontSize="large" />
+                  <Text
+                    fontSize={"small"}
+                    fontWeight={500}
+                    color={"#2C2C68"}
+                    ml={1}
+                  >
+                    This place has adjustments for hearing impairments
+                  </Text>
+                </Flex>
+              )}
+              {cardData.acessibility?.brain && (
+                <Flex
+                  alignItems={"center"}
+                  width={"full"}
+                  px={1}
+                  height={"25%"}
+                  // bg="yellow"
+                >
+                  <PsychologyIcon fontSize="large" />
+                  <Text
+                    fontSize={"small"}
+                    fontWeight={500}
+                    color={"#2C2C68"}
+                    ml={1}
+                  >
+                    This place has a quiet space
+                  </Text>
+                </Flex>
+              )}
+            </Flex>
           </Box>
         )}
       </GoogleMapReact>
@@ -192,6 +245,3 @@ const Map = ({ coordinates, setCoordinates, setBounds, places, place}) => {
 };
 
 export default Map;
-
-
-
