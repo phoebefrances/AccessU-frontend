00f75928372ba144f963f76dfdea67cabb1f95ea
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-08-07 13:10:44 +0100

    Changed colour of pin back to #2C2C68. For some reason still doesn't display porperly. Changed offlineData.jsx so that now we have 5 restaurants each with different name, lat/long, website, address, picutre, rating and accessibility badges. Other venue categories (gym, etc.) still need changing

--- frontend/components/Map.jsx
+++ frontend/components/Map.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Box, Image, Spacer, Text, Flex } from "@chakra-ui/react";
 import GoogleMapReact from "google-map-react";
-import GoogleMap from "google-map-react"
+import GoogleMap from "google-map-react";
 import { IoLocation } from "react-icons/io5";
 import LargeCard from "./LargeCard";
 
@@ -22,13 +22,12 @@ const Map = ({ coordinates, setCoordinates, setBounds, places, place }) => {
         defaultZoom={10}
         margin={[50, 50, 50, 50]}
         options={{
-          styles: require('../libs/map-style1.json')
-      }}
+          styles: require("../libs/map-style1.json"),
+        }}
         onChange={(e) => {
           setCoordinates({ lat: e.center.lat, lng: e.center.lng });
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
-
         //added click event by setting setCardData to places and passed the child as an array to places to get the index number (not getting an object) and set setisCard to true
         onChildClick={(child) => {
           setCardData(places[child]);
@@ -39,16 +38,15 @@ const Map = ({ coordinates, setCoordinates, setBounds, places, place }) => {
           setIsCard(false);
         }}
       >
-    
-
         {places?.map((place, i) => (
-          <Box key={i}
+          <Box
+            key={i}
             lat={Number(place.latitude)}
             lng={Number(place.longitude)}
             position={"relative"}
             cursor="pointer"
           >
-            <IoLocation color="#00396" fontSize={30} />
+            <IoLocation color="#2C2C68" fontSize={30} />
           </Box>
         ))}
 
