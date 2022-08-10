c21ad0f945c656814748c0e41d9c0356ea6bae82
kendallgmason <93265601+kendallgmason@users.noreply.github.com>
2022-08-10 12:22:02 +0100

    updated pass in data for large card web address

--- frontend/components/LargeCard.jsx
+++ frontend/components/LargeCard.jsx
@@ -96,7 +96,7 @@ const LargeCard = ({ cardData }) => {
           isTruncated
           color="#2C2C68"
         >
-        <a href={cardData.web_address}>
+        <a href="https://${cardData.web_address}">
           {cardData.web_address}
        </a>
         </Text>
