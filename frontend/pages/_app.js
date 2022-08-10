986f39fbd12287487e6edb7176252b3c039253c2
Phantom <sambaeverything@gmail.com>
2022-08-10 14:58:51 +0200

    Add and install TagManger module, to use with Google Tag manager, so we can use the Walkthrough function

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from 'react';
+import './walkthrough.css'
 
+import TagManager from 'react-gtm-module'
 function MyApp({ Component, pageProps }) {
+  const tagManagerArgs = {
+    gtmId: 'GTM-K6FCPNV'
+}
+
+if (process.browser) {
+  TagManager.initialize(tagManagerArgs);
+}
   return (
     <ChakraProvider>
       <Component {...pageProps} />
