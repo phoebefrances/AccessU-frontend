68695f10edb0339b5323b77c7d371dd4234b8a8d
Phoebe Windham <phoebe.frances@outlook.com>
2022-08-11 15:36:49 +0100

    added styling to components

--- frontend/pages/About.js
+++ frontend/pages/About.js
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 import Header from "../components/Header";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import { QuestionOutlineIcon } from "@chakra-ui/icons";
+import { fontWeight } from "@mui/system";
 
 // https://final-project-front-end-undefinedd.vercel.app/
 
@@ -21,9 +22,31 @@ export default function About() {
     <div>
       <div>
         <Head>
-          <title>About | Enabled </title>
+        <title>About | Enabled </title>
+        <meta name='description' content = 'About'/>
         </Head>
-        <Flex cursor="pointer" align="center">
+        <style jsx>{`
+            h1 {
+              color: #2C2C68;
+              font-weight: bold;
+              font-size: 25px;
+            }
+            h2 {
+              color: #2C2C68;
+              font-weight: bold;
+              font-size: 25px;
+              text-align: center,
+            }
+            li {
+                color: #2C2C68;
+                font-size: 16px;
+            }
+            p {
+                color: #2C2C68;
+                font-size: 16px;
+            }
+          `}</style>
+        <Flex cursor="pointer" object-fit = 'cover' object-position= 'center' >
           <Image
             src={Logo}
             alt={"logo"}
@@ -33,31 +56,47 @@ export default function About() {
         </Flex>
         <Flex>
           <QuestionOutlineIcon color={`#FF9100`} w={20} h={12} />
-          <p> Do you have to spend ages planning your trip? </p>
-          <style jsx>{`
-            .container {
-              margin: 50px;
-            }
-            p {
-              color: navy;
-              font-weight: 600;
-              font-size: 16px;
-            }
-          `}</style>
+         
+          <h1> Do you have to spend ages planning your trip? </h1>
 
           <QuestionOutlineIcon color={`#FF9100`} w={20} h={12} />
-          <p>
+          <h1>
             Have you visited somewhere to only find the access wasn't as you
             thought?
-          </p>
+          </h1>
 
           <QuestionOutlineIcon color={`#FF9100`} w={20} h={12} />
-          <p>
+          <h1>
             Are you anxious visiting a new place so much so it stopped you from
             making that trip?
-          </p>
+          </h1>
         </Flex>
+
+
+        <main>
+                <br/>
+                <h2>Welcome!</h2>
+              
+                <p>Enabled is here to take the stress out of planning. Our aim is to provide you with all the accessibility information you need in order to plan your trip.</p>
+                <br/>
+
+                <h2>Why?</h2>
+           
+                <ol>
+                    <li>1 in 4 households are impacted by disability</li>
+                    <li>98% of disabled people look for accessibility information before visiting somewhere for the first time</li>
+                    <li>Over 75% of disabled people don’t visit somewhere they wanted to go because they couldn’t find the accessibility information they needed</li>
+                    <li>This results in disabled people making 26% fewer trips than those without a disability</li>
+                </ol>
+                <br/>
+           
+                <p>With Enabled we hope you can be more spontaneous, visit, recommend and return!</p>
+            </main>
       </div>
     </div>
   );
 }
+
+
+
+
