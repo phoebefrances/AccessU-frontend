b696c3ff575f66433532a5f5b75334ecb450ecbd
kendallgmason <93265601+kendallgmason@users.noreply.github.com>
2022-08-11 11:32:25 +0100

    Testing file extension name fixed

--- Tests/Search.test.jsx
+++ Tests/Search.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen  } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import "@jest/globals";
-import Search from '../frontend/components/Search.js';
+import Search from '../frontend/components/Search.jsx';
 
 describe('Checks to see if the Search placeholder text renders', () => {
   it('Checks to see if the Search placeholder text renders', () => {
