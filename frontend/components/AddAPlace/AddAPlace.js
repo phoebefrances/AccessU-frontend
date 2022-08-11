c355c75e26c7940696f325407ac437a66792a48b
Phoebe Windham <phoebe.frances@outlook.com>
2022-08-11 23:11:01 +0100

    created addaplace component

---
+++ frontend/components/AddAPlace/AddAPlace.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import AddAPlaceForm from './AddAPlaceForm';
+import './AddAPlace.module.css';
+
+const AddAPlace = () => {
+  return (
+    <div className={"new-entry"}>
+      <AddAPlaceForm />
+    </div>
+  );
+};
+
+export default AddAPlace;
